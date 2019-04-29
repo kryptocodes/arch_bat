2f3a2808a2891eafcb4a7699f40bc2f8f334d3a9
srivatsan tb <40030019+kryptocodes@users.noreply.github.com>
2019-04-29 20:35:17 +0530

    initial commit

---
+++ arch.bat
@@ -0,0 +1,10 @@
+#to install arch linux 
+@REM https://github.com/hakuna-m/wubiuefi/blob/master/src/wubi/application.py
+@ECHO OFF
+TITLE ARCH - Arch linux Installer For Windows
+#download wubi latest version and keep in the same dir
+ECHO Enter the url of arch linux link(ISO ONLY)
+SET /P ArchISOU= 
+bitsadmin /transfer archiso "%ArchISOU%" "%cd%\arch.iso"
+ECHO Launching... (NOTE: It dosen't install ubuntu, It's using the arguments to replace ISO and Distro.)
+%cd%\wubi.exe --distro Arch --isopath %cd%\arch.iso
