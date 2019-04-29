30f2fab23a0eee82a3b9d7b8a9778c177c1bc671
srivatsantb <srivatsantb@gmail.com>
2019-04-29 22:42:20 +0530

    updated
    Signed-off-by: srivatsantb <srivatsantb@gmail.com>

--- arch.bat
+++ arch.bat
@@ -6,5 +6,6 @@ TITLE ARCH - Arch linux Installer For Windows
 ECHO Enter the url of arch linux link(ISO ONLY)
 SET /P ArchISOU= 
 bitsadmin /transfer archiso "%ArchISOU%" "%cd%\arch.iso"
-ECHO Launching... (NOTE: It dosen't install ubuntu, It's using the arguments to replace ISO and Distro.)
+ECHO Launching... (NOTE:It dosen't install ubuntu, It's using the arguments to replace ISO and Distro.)
+#it changes ubuntu to arch 
 %cd%\wubi.exe --distro Arch --isopath %cd%\arch.iso
